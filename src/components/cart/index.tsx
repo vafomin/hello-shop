0f2515147c2e456867afedee201992035a461d9f
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-06-12 16:44:54 +0300

    fix: increase z-index for cart

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -33,7 +33,7 @@ const Cart: React.FC<CartProps> = (props) => {
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 overflow-hidden"
+        className="fixed z-20 inset-0 overflow-hidden"
         onClose={onClose}
       >
         <div className="absolute inset-0 overflow-hidden">
