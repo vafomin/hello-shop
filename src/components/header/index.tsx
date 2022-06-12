b2564ed2a2c8ff3eb46e0394da364fa305e665b1
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-06-12 05:55:36 +0300

    fix: cart button position

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ const Header: React.FC<HeaderProps> = (props) => {
   const { cartSize, onOpen } = props;
 
   return (
-    <nav className="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+    <nav className="font-sans flex text-left sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
       <div className="mb-2 sm:mb-0 inner">
         <a
           href="/"
@@ -21,7 +21,7 @@ const Header: React.FC<HeaderProps> = (props) => {
         <span className="text-xs text-grey-dark">Simple react shop</span>
       </div>
 
-      <div className="sm:mb-0 self-center">
+      <div className="sm:mb-0 self-center ml-auto">
         <button
           id="cart-button"
           className="inline-flex items-center bg-blue-500 rounded-full font-bold text-white px-4 py-3 transition duration-300 ease-in-out hover:bg-blue-600 mr-6"
